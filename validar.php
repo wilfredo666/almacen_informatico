7bc33227bf8f47547062cf90ebcc854584c5a578
wilfredo666 <wilfredosaez@hotmail.com>
2020-11-10 12:30:14 -0400

    10-11-20
    Panle de control

---
+++ validar.php
@@ -0,0 +1,18 @@
+<?php
+$usuario=$_POST['usuario'];
+$clave=$_POST['clave'];
+$conexion=mysqli_connect("localhost","root","","almacen_informatico");
+/*$conexion=mysqli_connect("mysqlcluster25","costos","Admin123","grupo_regional");*/
+$consulta="select * from usuario where nombre_usu='$usuario' and password='$clave'";
+$resultado=mysqli_query($conexion,$consulta);
+$filas=mysqli_fetch_row($resultado);
+if($filas>0){
+    header("Location:deashboard/index.html?id=$filas[0]");
+}
+else{
+    echo "Error de autenticacion";
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+?>
